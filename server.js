ea4f4e7264938b6389358807c0d4e4f63cbcf36c
FreezingKas <57356272+FreezingKas@users.noreply.github.com>
2022-10-21 19:13:41 +0200

    minor changes

--- server.js
+++ server.js
@@ -6,21 +6,22 @@ const app = express()
 const port = 3000
 
 
-//config diverses
+// mis config
 app.use(express.json())
 app.use(bodyParser.json());
 
+// get docker routes
 require("./routes/docker.route.js")(app);
 
 // Run backgournd task to delete expired containers
 setInterval(dockerModel.deleteExpiredContainer.bind(null, 60), 60000);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(__dirname + './front/index.html');
 })
 
 app.listen(port,"127.0.0.1", () => {
-  console.log(` MAK'HACK Docker Orchestrer Express App on port : ${port}`)
+  console.log(`MAK'HACK Docker Orchestrer Express App on port ${port}`)
 });
 
 
